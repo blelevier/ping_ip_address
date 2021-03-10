5a981554c1ccb7818f6cbb144403f4481b14d093
blelevier <bylelevier@gmail.com>
2021-03-09 18:30:03 -0700

    feat: Add first version

---
+++ README.md
@@ -0,0 +1,48 @@
+# Ping IP Address
+
+My solution for the programming challenge.
+
+## Requirements
+
+Ping the IP address ranges 192.168.1.0/24 and 192.168.2.0/24 and report all IP addresses that are pingable on one range, but not on the other. For example, if we find that 192.168.1.34 is pingable, but 192.168.2.34 is not pingable, then that's a case we want to know about. Similarly if 192.168.2.34 is pingable, but 192.168.1.34 is not pingable, we also need to know about that.
+
+Additional important requirements:
+- You must use python3.6 or higher.
+- Your code needs to run quickly, so some kind of multi-threading, multi-tasking, whatever you want to use, is needed.</br>
+Implemented using multiprocessing package. In my machine it is running in less than a minute average. Considering all ip addresses are timed-out.
+- You will need to retry IP addresses that fail on their first ping. The number of retry attempts is up to you.</br>
+Attempting twice.
+- Your code MUST have no PEP8 issues and MUST include comments and docstrings where appropriate.
+- Please include mock-tests, unit-tests, any kind of tests that you write to help prove that your code behaves as expected.</br>
+As those addresses are not reachable in my computer, first I tested the program for known addresses like google.com or any website. Then, to test the selection of the ip addresses, I just introduced a dict with some predefined values to see if the parsing was working.
+- As we are an Ubuntu/Linux shop, implementing it there (or maybe macOS) would be preferred but not absolutely required.</br>
+Developed on Windows 10, but also tested on Ubuntu 18.04 virtual machine.
+-BONUS: Allow specific IP addresses to be skipped based on their last octet, so for example we could exclude 192.168.1.56 and 192.168.2.56 by specifying '56' to some function in your code,
+
+## Instructions
+
+1. Clone the project repository:</br>
+```
+git clone https://github.com/blelevier/ping_ip_address.git
+```
+
+2. Change directory and run the file:</br>
+```
+python3 ping.py
+```
+You can skip specific IP addresses with `python3 ping.py arg1 arg2 arg3` by specifying their last octet instead of arg1, arg2, ..., argn. For example this line will skip pairs [192.168.1.58, 192.168.2.58], [192.168.1.123, 192.168.2.123] and [192.168.1.237, 192.168.2.237].
+```
+python3 ping.py 58 123 237
+```
+
+## Results
+
+</br>
+<img src="images/img1.png"/>
+
+## Future Work 
+
+- Analize complexity and explore other approaches to multi-threading/tasking to make it faster and cleaner (removing the global variable).
+- Explore maximum number of available cores to increase the number of threads.
+- Get more familiar with wireshark, tcpdump.
+
---
+++ ping.py
@@ -0,0 +1,137 @@
+import multiprocessing
+import multiprocessing.dummy
+import subprocess
+import sys
+import time
+
+from constants import BASE1, BASE2
+
+my_dict1 = {}  # Global Variable (TODO: Find a cleaner approach)
+
+
+def skip_address(skippers, ip):
+    """Allow specific IP addresses to be skipped according to user input.
+    Args:
+        skippers (list): Octets of the ip addresses to be skipped
+        ip (list): Original list of ip addresses to be pinged
+    Returns:
+        ip (list): Original list minus the skipped numbers.
+    """
+    for number in skippers:
+        ip.remove(BASE1 + number)
+        ip.remove(BASE2 + number)
+    return ip
+
+
+def ping(ip):
+    """Run the console command to ping the ip addresses twice. Output
+    from the terminal is not displayed, as there is a lot of information.
+    After that, add the ip address to a dict, being the key the ip address
+    and the value "Yes" or "No", depending if the connection was successful.
+    Args:
+        ip (list): list of ip addresses to be pinged
+    Return:
+        ping_reply.returncode (bool): Depending if ping was successful or not
+    """
+    ping_reply = subprocess.run(["ping", "-n", "2", ip],
+                                stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+    if ping_reply.returncode == 0:
+        my_dict1[ip] = "Yes"
+    else:
+        my_dict1[ip] = "No"
+    return ping_reply.returncode
+
+
+def start_pinging(ip):
+    """
+    Initialize a number of threads (maybe we can run more than this?).
+    Implemented using the multiprocessing package, using the basic
+    example of data parallellism with Pool.
+    Args:
+        ip (list): list of ip addresses to be pinged
+    """
+    num_threads = 15 * multiprocessing.cpu_count()
+    p = multiprocessing.dummy.Pool(num_threads)
+    p.map(ping, ip)
+
+
+def find_differences():
+    """
+    Compare the value in the dictionary to detect which one are different.
+    For example: '192.168.1.55' vs '192.168.2.55'
+    Return:
+        octet (list): ip addresses that have different values.
+    """
+    octet = []
+    for i in range(256):
+        try:
+            if my_dict1[BASE1 + str(i)] != my_dict1[BASE2 + str(i)]:
+                octet.append(str(i))
+        except KeyError:
+            pass
+
+    return octet
+
+
+def print_final_list(octets, my_dict1):
+    """
+    Display the final output of the program. In other words, print the
+    list of the pair of ip addresses that had different values.
+    Args:
+        my_dict1 (dict): key=ip address, value= "Yes" or "No"
+        octet (list): ip addresses that have different values.
+    """
+    if not octets:
+        print("There were no addresses that were pingable on one range,"
+              "but not on another.")
+    else:
+        print("IP addresses that are pingable on one range, but not on"
+              "the other one:")
+        for octet in octets:
+            print(octet.rjust(3) + " --> " + BASE1 + octet +
+                  "\t= " + my_dict1[BASE1 + octet] + ",\t" +
+                  BASE2 + octet + "\t= " + my_dict1[BASE2 + octet])
+
+
+def test_connection():
+    """Ping two well-known sites like Google and Yahoo just to verify the
+    ping command is working as expected in your distribution.
+    """
+    print("************************************************************")
+    print("Pinging Google and Yahoo to test connection...")
+    ip3 = ["google.com", "yahoo.com"]
+    for ip in ip3:
+        ping_reply = subprocess.run(["ping", "-n", "2", ip])
+        if ping_reply.returncode == 0:
+            print("Success pinging " + ip)
+        else:
+            print("No respone from " + ip + ". Please review your connection.")
+    print("************************************************************")
+
+
+def main():
+    start = time.time()
+    print("Running...")
+    test_connection()
+
+# Create list of ip addresses
+    ip_address1 = [BASE1 + "{}".format(i) for i in range(0, 256)]
+    ip_address2 = [BASE2 + "{}".format(i) for i in range(0, 256)]
+    joint_list_ip = ip_address1 + ip_address2
+
+# Read command line arguments to decide which ip addresses to skip
+    if len(sys.argv) > 1:
+        to_skip = sys.argv[1:]
+        joint_list_ip = skip_address(to_skip, joint_list_ip)
+
+# Start pinging
+    start_pinging(joint_list_ip)
+    octet_list = find_differences()
+    print_final_list(octet_list, my_dict1)
+
+    print("\nRunning time: " + str(time.time() - start) + " seconds")
+
+
+if __name__ == "__main__":
+    main()
